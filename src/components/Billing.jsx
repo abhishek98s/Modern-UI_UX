f4a43599b3f40d02dec970fda46d972f80c5fb31
Abhishek <abhishekshakya98.com@gmail.com>
2023-01-30 21:32:43 +0545

    initial commit

---
+++ src/components/Billing.jsx
@@ -0,0 +1,39 @@
+import { apple, bill, google } from "../assets/assets";
+import styles, { layout } from "../styles";
+
+function Billing() {
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your
+          <hr className="sm:block hidden" />
+          billing & invoicing.w
+        </h2>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Elit enim sed mass etai . Mauriis eu adiposcing ultrices ametodio
+          aemean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.w
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+          <img src={google} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Billing;
---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,23 @@
+import {card} from '../assets/assets';
+import styles, {layout} from '../styles';
+import Button from './Button';
+
+function CardDeal() {
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu torto, purus in mattis at sed iinteger faucibus. 
+        Aliquet quis aliquet eget mauris tortor. Aliquet quis aliquet eget mauris tortor.</p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  )
+}
+
+export default CardDeal
